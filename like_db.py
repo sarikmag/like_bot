121964214275934ebac95783dddb0956d4dd7513
sarikmag <sardorbek.abdujalilov@mail.ru>
2023-01-23 09:55:31 +0500

    lke bot

--- like_db.py
+++ like_db.py
@@ -18,19 +18,43 @@ class LikeDB:
         with open(self.db_path, 'w') as f:
             json.dump(self.db, f, indent=4)
     
+    def add_user(self, user_id):
+        # with open(self.db_path, 'w') as f:
+        #     json.dump(self.db, f, indent=4)
+        # self.db.append(user_id:{
+        #     "likes":0,
+        #     "dislikes":0
+        # })
+        users = self.db.keys()
+        users_dict = {}
+        for i in users:
+            users_dict[i]=0
+        add = users_dict.get(user_id, 'net')
+        if add == 'net':
+            self.db[user_id]={}
+            self.db[user_id]['likes']=0
+            self.db[user_id]['dislikes']=0
+        self.save()
+    
     def all_likes(self):
         """Counts all users likes
         returns
             all users likes
         """
-        pass
+        likes=0
+        for like in self.db.keys():
+            likes+=self.db[like]['likes']
+        return likes
         
     def all_dislikes(self):
         """Counts all users dislikes
         returns
             all users dislikes
         """
-        pass
+        dislikes=0
+        for dislike in self.db.keys():
+            dislikes+=self.db[dislike]['dislikes']
+        return dislikes
         
         
     #Add a like to the database
@@ -42,7 +66,15 @@ class LikeDB:
         returns:
             The number of likes and dislikes for the post
         '''
-        pass
+        if self.db[user_id]['likes']==0 and self.db[user_id]['dislikes']==0:
+            self.db[user_id]['likes']=1
+        elif self.db[user_id]['likes']==1:
+            self.db[user_id]['likes']=0
+        elif self.db[user_id]['dislikes']==1:
+            self.db[user_id]['dislikes']=0
+            self.db[user_id]['likes']=1
+        self.save()
+        
 
   
     #Add a dislike to the database
@@ -54,4 +86,18 @@ class LikeDB:
         returns:
             The number of likes and dislikes for the post
         '''
-        pass
+        if self.db[user_id]['likes']==0 and self.db[user_id]['dislikes']==0:
+            self.db[user_id]['dislikes']=1
+        elif self.db[user_id]['likes']==1:
+            self.db[user_id]['dislikes']=1
+            self.db[user_id]['likes']=0
+        elif self.db[user_id]['dislikes']==1:
+            self.db[user_id]['likes']=0
+            self.db[user_id]['dislikes']=0
+        self.save()
+
+# likedb = LikeDB('likedb.json')
+# print(likedb.all_likes())
+# print(likedb.all_dislikes())
+# print(likedb.add_like('795394603'))
+# print(likedb.add_user('756243577'))
--- likebot.py
+++ likebot.py
@@ -2,20 +2,76 @@
 from telegram.ext import Updater,CommandHandler,CallbackContext,MessageHandler,Filters,CallbackQueryHandler
 from telegram import Update,ReplyKeyboardMarkup,KeyboardButton,InlineKeyboardMarkup,InlineKeyboardButton
 from like_db import LikeDB
+import os
+
+TOKEN = os.environ['TOKEN']
+
+likedb = LikeDB('likedb.json')
 
 #Import TOKEN from envoirment variable
-TOKEN = '5549407151:AAGVaQx5L2bvwBYnZE3a50yycdBQfPvl1fo'
+# TOKEN = '5962805915:AAFQO0SG2RVhTQhbMN9FPYjDnLMC_BpfVt0'
 
 updater = Updater(TOKEN)
 #Create start command handler
 def start(update:Update, context:CallbackContext):
     """Starts with picture all likes and all dislikes"""
+    chat_id = update.message.chat.id
+    likedb.add_user(chat_id)
+    keyboar = ReplyKeyboardMarkup([
+        ['Send photo']
+    ])
+    bot = context.bot
+    bot.sendMessage(
+    chat_id=chat_id,
+    text='Assalom alaykum xush kelibsiz botimizga 👍',
+    reply_markup=keyboar
+    )
     
+
+def send_photo(update:Update, context=CallbackContext):
+    chat_id = update.message.chat.id
+    likedb.add_user(chat_id)
+    bot = context.bot
+    text_likes = f'👍 {likedb.all_likes()}'
+    text_dislikes = f'👎 {likedb.all_dislikes()}'
+    like=InlineKeyboardButton(text=text_likes,callback_data='👍')
+    dislike=InlineKeyboardButton(text=text_dislikes,callback_data='👎')
+    keyboard=InlineKeyboardMarkup([
+        [like,dislike]
+    ])
+    bot.sendPhoto(chat_id=chat_id,photo='https://avatars.mds.yandex.net/i?id=1ab9c215cc4eff5b225dacfa12a6d52b6764c3c3-5347144-images-thumbs&n=13',reply_markup=keyboard)
+
+def quer(update:Update, context=CallbackContext):
+    query = update.callback_query
+    chat_id = query.message.chat_id
+    # message_id = query.message_id
+    data = query.data
+    bot = context.bot
+    # text_likes = f'👍 {likedb.all_likes()}'
+    # text_dislikes = f'👎 {likedb.all_dislikes()}'
+    # like=InlineKeyboardButton(text=text_likes,callback_data='👍')
+    # dislike=InlineKeyboardButton(text=text_dislikes,callback_data='👎')
+    # keyboard=([
+    #     [like,dislike]
+    # ])
+    if data == "👍":
+        likedb.add_like(chat_id)
+        
+
+    elif data == "👎":
+        likedb.add_dislike(chat_id)
+        
+    # bot.edit_message_reply_markup(chat_id=chat_id, message_id=message_id, reply_markup=keyboard)
+
+    query.answer('You are choosed')
+
 #Create updater and dispatcher
-updater = Updater(TOKEN)
+updater = Updater(token=TOKEN)
 
 #add handler to updater
-updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('start',start))
+updater.dispatcher.add_handler(MessageHandler(Filters.text('Send photo'), send_photo))
+updater.dispatcher.add_handler(CallbackQueryHandler(quer))
 
 #Start the bot
 updater.start_polling()
